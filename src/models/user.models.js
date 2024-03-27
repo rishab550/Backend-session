4c3b9bc2741be5f442498c39d660229e7e154e49
Rishabh Garg <you@example.com>
2024-03-27 17:06:45 +0530

    All models added

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -3,7 +3,6 @@ import { Schema } from "mongoose";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 
-
 const userSchema = new Schema(
   {
     username: {
@@ -12,7 +11,7 @@ const userSchema = new Schema(
       unique: true,
       lowercase: true,
       trim: true,
-      index: true
+      index: true,
     },
 
     email: {
@@ -27,78 +26,79 @@ const userSchema = new Schema(
       type: String,
       required: true,
       trim: true,
-      index: true
+      index: true,
     },
     avatar: {
-      type: String,  // Cloudinary URL,
+      type: String, // Cloudinary URL,
       required: true,
     },
 
     coverImage: {
-      type: String,  // Cloudinary URL,
-
+      type: String, // Cloudinary URL,
     },
 
     watchHistory: {
       type: Schema.Types.ObjectId,
-      ref: "Video"
+      ref: "Video",
     },
 
     password: {
       type: String,
-      required: [true, "Password is required"]
+      required: [true, "Password is required"],
     },
 
     refreshToken: {
-      type: String
-    }
-  },{timestamps: true})
+      type: String,
+    },
+  },
+  { timestamps: true },
+);
 
-  userSchema.pre("save", async function (next) {
-    if(!this.isModified("password")) return next();
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
 
-    this.password = await bcrypt.hash(this.password, 10)
-    next()
-})
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
 
-userSchema.methods.isPasswordCorrect = async function(password){
-    return await bcrypt.compare(password, this.password)
-}
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
-userSchema.methods.isPasswordCorrect = async function(password) {
-  console.log('Input password:', password);
-  console.log('Stored hashed password:', this.password);
-  
+userSchema.methods.isPasswordCorrect = async function (password) {
+  console.log("Input password:", password);
+  console.log("Stored hashed password:", this.password);
+
   const isCorrect = await bcrypt.compare(password, this.password);
-  console.log('Password comparison result:', isCorrect);
-  
+  console.log("Password comparison result:", isCorrect);
+
   return isCorrect;
-}
+};
 
-  userSchema.methods.generateAccessToken = function (){
-   return jwt.sign(
-      {
-        _id: this._id,
-        email: this.email,
-        username: this.username,
-        fullname: this.fullname
-      },
-      process.env.ACCESS_TOKEN_SECRET,
-      {
-        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
-      }
-    )
-  }
-  userSchema.methods.generateRefreshToken = function (){
-    return jwt.sign(
-      {
-        _id: this._id,
-      },
-      process.env.REFRESH_TOKEN_SECRET,
-      {
-        expiresIn: process.env.REFRESH_TOKEN_EXPIRY
-      }
-    )
-  }
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    },
+  );
+};
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    },
+  );
+};
 
-export const User = mongoose.model("User", userSchema) 
+export const User = mongoose.model("User", userSchema);
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,66 +1,80 @@
 import { Router } from "express";
-import registerUser, {     changeCurrentPassword, 
-  getCurrentUser, 
-  getUserChannelProfile, 
-  getWatchHistory, 
-  loginUser, 
-  logoutUser, 
-  refreshAccessToken, 
-  updateAccountDetails, 
-  updateUserAvatar, 
-  updateUserCoverImage } from "../controllers/user.controller.js";
+import registerUser, {
+  changeCurrentPassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  getWatchHistory,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { updateThumbnail, uploadVideo } from "../controllers/video.controller.js";
+import {
+  updateThumbnail,
+  uploadVideo,
+} from "../controllers/video.controller.js";
 
+const router = Router();
 
+router.route("/register").post(
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser,
+);
 
-const router = Router()
-
-router.route("/register").post(upload.fields([
-  {
-    name: "avatar",
-    maxCount: 1
-  },
-  {
-    name: "coverImage",
-    maxCount: 1
-  }
-]),registerUser)
-
-
-router.route("/login").post(loginUser)
+router.route("/login").post(loginUser);
 
 //secured routes
-router.route("/logout").post(verifyJWT, logoutUser)
+router.route("/logout").post(verifyJWT, logoutUser);
 
-router.route("/refresh-token").post(refreshAccessToken)
+router.route("/refresh-token").post(refreshAccessToken);
 
-router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
 
-router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/current-user").get(verifyJWT, getCurrentUser);
 
-router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
 
-router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
 
-router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+router
+  .route("/cover-image")
+  .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
 
-router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
 
-router.route("/watch-history").get(verifyJWT, getWatchHistory)
+router.route("/watch-history").get(verifyJWT, getWatchHistory);
 
-router.route("/upload-video").post(upload.fields([
-  {
-    name: "videoFile",
-    maxCount: 1
-  },
-  {
-    name: "thumbnail",
-    maxCount: 1
-  }
-]),uploadVideo)
+router.route("/upload-video").post(
+  upload.fields([
+    {
+      name: "videoFile",
+      maxCount: 1,
+    },
+    {
+      name: "thumbnail",
+      maxCount: 1,
+    },
+  ]),
+  uploadVideo,
+);
 
-router.route("/update-thumbnail").patch(verifyJWT, upload.single("thumbnail"), updateThumbnail)
+router
+  .route("/update-thumbnail")
+  .patch(verifyJWT, upload.single("thumbnail"), updateThumbnail);
 
 export default router;
